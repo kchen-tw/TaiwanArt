b4d5da0256b112f00dc3f036a864bd296498af04
Kun Chen <kchen@csie.org>
2017-05-25 01:05:15 +0800

    TaiwanArt

---
+++ server.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var multer = require('multer');
+var path = require('path');
+var crypto = require('crypto');
+
+var app = new express();
+
+// 設定靜態網頁
+app.use(express.static(__dirname + '/public'));
+
+
+// get image from client
+var storage = multer.diskStorage({
+    destination: './uploads',
+    filename: function(req, file, cb) {
+        cb(null, file.originalname + '.jpg');
+    }
+});
+
+app.post('/imageupload', multer({
+    storage: storage
+}).single('upload'), function(req, res) {
+
+
+    console.log(req);
+    console.log(req.file.originalname);
+
+
+    var uploadImage = require("./image_generate.js");
+
+    var options = {
+        path: req.file.path,
+        filename: req.file.filename
+    }
+
+    uploadImage.generate(options, (data) => {
+        console.log(data);
+
+        var result = {
+            status: 'ok'
+        };
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        });
+        res.write(JSON.stringify(result));
+        res.end();
+    });
+
+    ///// execute style transfer pipieline start////////
+    // var exec = require('child_process').exec;
+    // var execProcess = require("./exec_process.js");
+    // execProcess.result("git do_pipeline_works.sh " + req.file.path + " " + __dirname + "/public/", function(err, response) {
+
+    //     if (!err) {
+    //         console.log(response);
+    //         var result = {
+    //             status: 'ok'
+    //         };
+    //         res.writeHead(200, {
+    //             'Content-Type': 'application/json',
+    //             'Access-Control-Allow-Origin': '*'
+    //         });
+    //         res.write(JSON.stringify(result));
+    //         res.end();
+    //     } else {
+    //         console.log(err);
+    //     }
+    // });
+    /////////////////////////////////////////////////////
+
+
+});
+
+app.listen(3000, console.log("Listening on port 3000"));
