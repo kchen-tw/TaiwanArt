58981f1c63277a5d0882c88aea2e976a11998086
Kun Chen <kchen@csie.org>
2017-06-01 22:47:39 +0800

    use vue.js loaded data

--- public/js/index-vue.js
+++ public/js/index-vue.js
@@ -1,150 +1,148 @@
 'use strict';
 
-;
-(function(document, window, index) {
-    // feature detection for drag&drop upload
-    var isAdvancedUpload = function() {
-        var div = document.createElement('div');
-        return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
-    }();
-
-
-    // applying the effect for every form
-    var forms = document.querySelectorAll('.box');
-    Array.prototype.forEach.call(forms, function(form) {
-        var input = form.querySelector('input[type="file"]'),
-            label = form.querySelector('label'),
-            errorMsg = form.querySelector('.box__error span'),
-            restart = form.querySelectorAll('.box__restart'),
-            droppedFiles = false,
-            showFiles = function(files) {
-                label.textContent = files.length > 1 ? (input.getAttribute('data-multiple-caption') || '').replace('{count}', files.length) : files[0].name;
-            },
-            triggerFormSubmit = function() {
-                var event = document.createEvent('HTMLEvents');
-                event.initEvent('submit', true, false);
-                form.dispatchEvent(event);
-            };
-
-        // letting the server side to know we are going to make an Ajax request
-        var ajaxFlag = document.createElement('input');
-        ajaxFlag.setAttribute('type', 'hidden');
-        ajaxFlag.setAttribute('name', 'ajax');
-        ajaxFlag.setAttribute('value', 1);
-        form.appendChild(ajaxFlag);
-
-        // automatically submit the form on file select
-        input.addEventListener('change', function(e) {
-            showFiles(e.target.files);
-
-
-        });
-
-        // drag&drop files if the feature is available
-        if (isAdvancedUpload) {
-            form.classList.add('has-advanced-upload'); // letting the CSS part to know drag&drop is supported by the browser
-
-            ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach(function(event) {
-                form.addEventListener(event, function(e) {
-                    // preventing the unwanted behaviours
-                    e.preventDefault();
-                    e.stopPropagation();
-                });
-            });
-            ['dragover', 'dragenter'].forEach(function(event) {
-                form.addEventListener(event, function() {
-                    form.classList.add('is-dragover');
-                });
-            });
-            ['dragleave', 'dragend', 'drop'].forEach(function(event) {
-                form.addEventListener(event, function() {
-                    form.classList.remove('is-dragover');
-                });
-            });
-            form.addEventListener('drop', function(e) {
-                droppedFiles = e.dataTransfer.files; // the files that were dropped
-                showFiles(droppedFiles);
-
-            });
-        }
-
-
-        // if the form was submitted
-        form.addEventListener('submit', function(e) {
-            // preventing the duplicate submissions if the current one is in progress
-            if (form.classList.contains('is-uploading')) return false;
-
-            form.classList.add('is-uploading');
-            form.classList.remove('is-error');
-
-            if (isAdvancedUpload) // ajax file upload for modern browsers
-            {
-                e.preventDefault();
-
-                // gathering the form data
-                var ajaxData = new FormData(form);
-                if (droppedFiles) {
-                    Array.prototype.forEach.call(droppedFiles, function(file) {
-                        ajaxData.append(input.getAttribute('name'), file);
-                    });
+// 當網頁載入完開始javascript
+$(function() {
+    var loader = new Loader($('.loader-wrapper'));
+    var painter_list = new Vue({
+        el: "#painter-list",
+        data: {
+            painters: [{
+                    "name": "陳澄波",
+                    "profileimg": "chen_chengpo.jpg",
+                    "homepage": "chen_chengpo.html",
+                    "dates": "台灣 （1895 - 1947）",
+                    "introduction": "1895，是個時代的改變，他來到了這世界；1945，他離開了，也是因為時代的改變。在他年輕的歲月裡，父母相繼過世，依靠叔父的他，每天除了上學，還要撿柴、撿蕃薯，但這沒改變他的求學熱情，長大後他反而成為了一個熱心助人的藝術家，以純真浪漫的性情，表現出了台灣迷人的特質。二二八事件後，他因參與和平使節，被他所認同的政府無情的槍決...",
+                    "paintings": [{
+                            "name": "淡水夕照",
+                            "imgurl": "https://i.imgur.com/xsWzJoW.jpg",
+                            "model": "OCT1_35"
+                        },
+                        {
+                            "name": "清流",
+                            "imgurl": "https://i.imgur.com/2A5poTz.jpg",
+                            "model": "OCS1_03"
+                        },
+                        {
+                            "name": "嘉義街景",
+                            "imgurl": "https://i.imgur.com/EETjohr.jpg",
+                            "model": "OCS1_03"
+                        },
+                        {
+                            "name": "玉山積雪",
+                            "imgurl": "https://i.imgur.com/ql0dvWK.jpg",
+                            "model": "OW1_05"
+                        }
+                    ]
+                },
+                {
+                    "name": "陳植棋",
+                    "profileimg": "chen_chihchi.jpg",
+                    "homepage": "chen_chihchi.html",
+                    "dates": "台灣 （1906 - 1931）",
+                    "introduction": "「人生是短促的，藝術才是永遠。」這是陳植棋再赴日留學歡送會上說的，沒想到，他卻親自實現了這句話。他具有梵谷的熱情、高更的睿智、羅特列克的放縱，卻與席勒一樣，英年早逝。",
+                    "paintings": [{
+                            "name": "自畫像",
+                            "imgurl": "https://i.imgur.com/WbKjU85.jpg",
+                            "model": "chenchihchi_self"
+                        },
+                        {
+                            "name": "台灣風景",
+                            "imgurl": "https://i.imgur.com/csXWZA3.jpg",
+                            "model": "chenchihchi_Taiwan"
+                        }
+                    ]
+                },
+                {
+                    "name": "石川欽一郎",
+                    "profileimg": "ishikawa_chinichiro.jpg",
+                    "homepage": "ishikawa_chinichiro.html",
+                    "dates": "日本 （1871 - 1945）",
+                    "introduction": "",
+                    "paintings": [{
+                            "name": "台中豐原道",
+                            "imgurl": "https://i.imgur.com/RUbHHTb.jpg",
+                            "model": "Ishikawa_Taichung"
+                        },
+                        {
+                            "name": "總督府",
+                            "imgurl": "https://i.imgur.com/drIvSH1.jpg",
+                            "model": "Ishikawa_taiwan"
+                        }
+                    ]
+                },
+                {
+                    "name": "倪蔣懷",
+                    "profileimg": "ni_chianghuai.jpg",
+                    "homepage": "ni_chianghuai.html",
+                    "dates": "台灣 （1894 - 1943）",
+                    "introduction": "1945，每一次的空襲，都是他的妻子，背著他與石川老師的畫作，逃跑；這些用生命保存下來的作品，給我們了一絲的機會，認識老畫家，還有他的年代。為什麼除了他的作品外，還有石川欽一郎的呢？原來倪蔣懷先生除了是台灣第一位水彩畫家外，更是第一代的收藏家。",
+                    "paintings": [{
+                        "name": "田寮港畔看基隆郵局",
+                        "imgurl": "https://i.imgur.com/1gg3u3Q.jpg",
+                        "model": "NI_Keelung02"
+                    }]
+                },
+                {
+                    "name": "黃榮燦",
+                    "profileimg": "huang_jungtsan.jpg",
+                    "homepage": "huang_jungtsan.html",
+                    "dates": "台灣 （1920 - 1952）",
+                    "introduction": "馬場町現在是台北河濱的親水公園，但在五十年前，他卻不是這樣悠閒的好去處。冤！冤！冤！他的學生說，這是他們最後聽到老師講的幾個字，然後就再也看不到他了，為了自保，他們有很長一段時間，不會說自己是“他”的學生。",
+                    "paintings": [{
+                            "name": "台灣農民作家楊逵之家",
+                            "imgurl": "https://i.imgur.com/Fnmb16k.jpg",
+                            "model": "Huang_Home_of_the_Farmer_Writer_Yang_Kui_in_Taiwan"
+                        },
+                        {
+                            "name": "恐怖的檢查",
+                            "imgurl": "https://i.imgur.com/d9WfQLr.jpg",
+                            "model": "Huang_Pullover"
+                        }
+                    ]
+                },
+                {
+                    "name": "黃土水",
+                    "profileimg": "huang_tushui.gif",
+                    "homepage": "huang_tushui.html",
+                    "dates": "台灣 （1895 - 1930）",
+                    "introduction": "",
+                    "paintings": [{
+                        "name": "婦女頭像",
+                        "imgurl": "https://i.imgur.com/c60xGGU.jpg",
+                        "model": "huangtushui_womwn"
+                    }]
+                },
+                {
+                    "name": "鹽月桃甫",
+                    "profileimg": "shiotsuki_toho.jpg",
+                    "homepage": "shiotsuki_toho.html",
+                    "dates": "日本 （1886 - 1954）",
+                    "introduction": "鹽月桃甫是末代武士的後代，因為民治維新而一貧如洗，身為么子的他，因為與祖父同天生日，被認為是祖父轉世，性命才得以保住。因為經濟需要而來台任教，從此開啟了他與台灣藝壇的情緣，特別是在藝術教育和原住民繪畫上。",
+                    "paintings": [{
+                        "name": "泰雅之女",
+                        "imgurl": "https://i.imgur.com/1D3EfjE.jpg",
+                        "model": "Shiotsuki_Taiyiwoman"
+                    }]
+                },
+                {
+                    "name": "鄉原古統",
+                    "profileimg": "gobara_koto.jpg",
+                    "homepage": "gobara_koto.html",
+                    "dates": "日本 （1887 - 1965）",
+                    "introduction": "",
+                    "paintings": [{
+                        "name": "新店溪",
+                        "imgurl": "https://i.imgur.com/cAlF8Ra.jpg",
+                        "model": "Gobara_HsinTein"
+                    }]
                 }
-
-                // ajax request
-                var ajax = new XMLHttpRequest();
-                ajax.open(form.getAttribute('method'), form.getAttribute('action'), true);
-
-                ajax.onload = function() {
-                    form.classList.remove('is-uploading');
-                    if (ajax.status >= 200 && ajax.status < 400) {
-                        var data = JSON.parse(ajax.responseText);
-                        form.classList.add(data.success == true ? 'is-success' : 'is-error');
-                        if (!data.success) errorMsg.textContent = data.error;
-                    } else alert('Error. Please, contact the webmaster!');
-                };
-
-                ajax.onerror = function() {
-                    form.classList.remove('is-uploading');
-                    alert('Error. Please, try again!');
-                };
-
-                ajax.send(ajaxData);
-            } else // fallback Ajax solution upload for older browsers
-            {
-                var iframeName = 'uploadiframe' + new Date().getTime(),
-                    iframe = document.createElement('iframe');
-
-                $iframe = $('<iframe name="' + iframeName + '" style="display: none;"></iframe>');
-
-                iframe.setAttribute('name', iframeName);
-                iframe.style.display = 'none';
-
-                document.body.appendChild(iframe);
-                form.setAttribute('target', iframeName);
-
-                iframe.addEventListener('load', function() {
-                    var data = JSON.parse(iframe.contentDocument.body.innerHTML);
-                    form.classList.remove('is-uploading')
-                    form.classList.add(data.success == true ? 'is-success' : 'is-error')
-                    form.removeAttribute('target');
-                    if (!data.success) errorMsg.textContent = data.error;
-                    iframe.parentNode.removeChild(iframe);
-                });
-            }
-        });
-
-
-        // restart the form if has a state of error/success
-        Array.prototype.forEach.call(restart, function(entry) {
-            entry.addEventListener('click', function(e) {
-                e.preventDefault();
-                form.classList.remove('is-error', 'is-success');
-                input.click();
-            });
-        });
-
-        // Firefox focus bug fix for file input
-        input.addEventListener('focus', function() { input.classList.add('has-focus'); });
-        input.addEventListener('blur', function() { input.classList.remove('has-focus'); });
-
-    });
-}(document, window, 0));
+            ],
+        }
+        // beforeCreate: function() {
+        //     loader.start();
+        // },
+        // ready: function() {
+        //     loader.end();
+        // }
+    })
+})
